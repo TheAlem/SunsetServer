487211a8e0b98d526498180ee35216b714d2acd5
Jose Alem Rodriguez Valverde <josealem03@gmail.com>
2023-12-06 08:42:36 -0400

    hecho

--- Historial.py
+++ Historial.py
@@ -23,6 +23,7 @@ client = pymongo.MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
 
+"""""
 # Función para obtener los últimos registros por tipo de dispositivo
 def obtener_ultimos_registros(tipo_dispositivo, limit=5):
     try:
@@ -31,6 +32,7 @@ def obtener_ultimos_registros(tipo_dispositivo, limit=5):
     except Exception as e:
         print(f"Error al obtener datos de MongoDB para {tipo_dispositivo}: {e}")
         return []
+"""
 
 # Funciones de callback para MQTT
 def on_connect(client, userdata, flags, rc):
@@ -40,21 +42,23 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, message):
     try:
-        mensaje_decodificado = str(message.payload.decode("utf-8","ignore"))
-        data = json.loads(mensaje_decodificado)
+        data = dict()
+        data['valor'] = str(message.payload.decode("utf-8","ignore"))
         tipo_dispositivo = message.topic.split('/')[-1]
         data['tipo_dispositivo'] = tipo_dispositivo
-
+        
         # Lógica específica para cada tópico
         if tipo_dispositivo == 'iluminacion':
             # Suponiendo que 'valor' es el campo en el mensaje para intensidad de iluminación
-            data['intensidad'] = int(data['valor'])
+            data['intensidad'] = data['valor']
         else:
             # Para otros tópicos que solo manejan encendido/apagado (0/1)
-            data['estado'] = int(data['valor'])
-
+            data['estado'] = data['valor']
+        
+        #print(json.dumps(data))
         collection.insert_one(data)
-        print(f"Datos guardados en MongoDB para {message.topic}")
+        #print(f"Datos guardados en MongoDB para {message.topic}")
+        
     except Exception as e:
         print(f"Error al procesar mensaje: {e}")
 
@@ -69,13 +73,14 @@ mqtt_client.loop_start()
 
 try:
     while True:
-        for tipo_dispositivo, topico in TOPICOS.items():
-            registros = obtener_ultimos_registros(tipo_dispositivo)
-            for registro in registros:
-                registro.pop('_id', None)  # Eliminar el campo _id de MongoDB para evitar errores de serialización
-                mensaje = json.dumps(registro, default=str)
-                mqtt_client.publish(topico, mensaje)
-        time.sleep(10)  # Intervalo de tiempo entre cada envío de datos
+        pass
+        #for tipo_dispositivo, topico in TOPICOS.items():
+            #registros = obtener_ultimos_registros(tipo_dispositivo)
+            #for registro in registros:
+                #registro.pop('_id', None)  # Eliminar el campo _id de MongoDB para evitar errores de serialización
+                #mensaje = json.dumps(registro, default=str)
+                #mqtt_client.publish(topico, mensaje)
+        #time.sleep(10)  # Intervalo de tiempo entre cada envío de datos
 except KeyboardInterrupt:
     print("Deteniendo el script...")
 
